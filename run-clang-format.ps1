6833a9687405000f60830f637b436857fb22b8c1
Tomas Mikalauskas <ttomas.mikalauskas@gmail.com>
2026-07-19 08:58:45 +0100

    fix: git lfs assets failing formatting

--- run-clang-format.ps1
+++ run-clang-format.ps1
@@ -8,17 +8,22 @@ if (-not $files) {
     exit 0
 }
 
+$formattedFiles = @()
 foreach ($f in $files) {
     Write-Host "Running clang-format on $f"
+    $contentBeforeFormatting = Get-Content -Raw -LiteralPath $f
     clang-format -i "$f"
     if ($LASTEXITCODE -ne 0) {
         Write-Error "clang-format failed on $f"
         exit 1
     }
+
+    if((Get-Conntent -Raw -LiteralPath $f) -cne $contentBeforeFormatting) {
+        $formattedFiles += $f
+    }
 }
 
-git diff --exit-code
-if ($LASTEXITCODE -ne 0) {
-    Write-Error "clang-format check failed: some files are not properly formatted. Please run clang-format locally and commit the changes."
+if ($formattedFiles.count -gt 0) {
+    Write-Error "clang-format updated the following files. Commit the formatting chages: " + ($formattedFiles -join ", ")
     exit 1
 }
